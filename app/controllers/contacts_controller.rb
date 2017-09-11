36e14716757d742690b5e32e20e25a1629e30e44
Jonathan Carter <joncarter96@gmail.com>
2017-09-11 18:33:20 +0000

    Tweaked contacts controller.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -20,6 +20,6 @@ class ContactsController < ApplicationController
   
   private
     def contact_params
-      params.require(:contact).permit(:name, :email, :comments)
+      params.require(:contact).permit(:name, :email, :body)
     end
 end
