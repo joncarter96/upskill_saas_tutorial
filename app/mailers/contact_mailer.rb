43673a9bc36069a836b97dd6eac253f446f7ba80
Jonathan Carter <joncarter96@gmail.com>
2017-09-11 21:25:13 +0000

    Contact mailer email message.

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,10 +1,10 @@
 class ContactMailer < ActionMailer::Base
   default to: 'joncarter96@gmail.com'
   
-  def contact_email(name, email, body)
+  def contact_email(name, email, comments)
     @name = name
     @email = email
-    @body = body
+    @body = comments
     
     mail(from: email, subject: 'Contact Form Message')
   end
